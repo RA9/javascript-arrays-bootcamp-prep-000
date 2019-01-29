880191b54d4ecec5a062e9f95f74ca0e1ee7ccdf
Carlos Nah <cnah27@gmail.com>
2019-01-29 15:33:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,9 @@ var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"];
 function  addElementToEndOfArray(array, element) {
   return [...array, element];
 }
+
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array
+}
